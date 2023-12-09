7e52f5ea1921b8b7cd0edef913eaab4f703df57e
Deepak <deep84chaudhary@gmail.com>
2023-12-10 01:01:34 +0530

    context hooks setup base url and for backend side config file setup done

--- client/src/context/BlogContext.jsx
+++ client/src/context/BlogContext.jsx
@@ -1,4 +1,6 @@
-import { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { BASE_URL } from "../config/config.js";
 
 // Create a context
 const BlogContext = createContext();
@@ -12,8 +14,24 @@ export function BlogProvider({ children }) {
 
   const [user, setUser] = useState({
     username: null,
-    email: null,
-  });
+    email: null
+  })
+
+  axios.defaults.withCredentials = true; // Global Credential
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get(`${BASE_URL}/`);
+        setUser(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+  
+    fetchUser();
+  }, []);
+  
 
   return (
     <BlogContext.Provider value={{ user }} >
